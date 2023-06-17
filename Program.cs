e7b2a7f0ec16427ee1f0461ce894971bc0c8d3b5
Johannes Norrbacka <johannes.norrbacka@hotmail.com>
2023-06-17 18:30:05 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd = new Random();
+var Answer = rnd.Next(1,99);
+Console.WriteLine("Gissa på en siffra mellan 1 och 99");
+Question:
+var Guess = Convert.ToInt32(Console.ReadLine());
+if (Answer==Guess)
+{
+    Console.WriteLine("Rätt!");
+}
+else
+{
+    Console.WriteLine("Fel!");
+    if (Answer>Guess)
+    {
+        Console.WriteLine("Gissa högre");
+    }
+    else 
+    {
+        Console.WriteLine("Gissa lägre");
+    }
+    goto Question;
+}
